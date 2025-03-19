b39ab2edb54fa158c1de301615c0817fb990bfbb
Ralamore <web24032@byui.edu>
2025-03-19 17:08:35 -0600

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   Recipies/index.html
    	modified:   Recipies/main.js
    	modified:   Recipies/recipes.css

--- Recipies/main.js
+++ Recipies/main.js
@@ -6,4 +6,62 @@ function getRandomIndex(maxNum) {
     return Math.floor(Math.random() * maxNum)
 }
 
-// alert(getRandomIndex(12))
+function getEntry(list) {
+    const listLength = list.length;
+    return list[getRandomIndex(listLength)];
+}
+
+alert(getEntry(recipes).author);
+
+function tagsTemplate(tags) {
+	// loop through the tags list and transform the strings to HTML
+
+	return html;
+}
+
+function recipeTemplate(recipe) {
+	return `<article class="recipe">
+            <img class="recipe-image" src="images/apple-crisp.jpg" alt="a bowl of apple crisp with ice cream">
+            <section id="rec-info">
+                <div class="tag-row">
+                    <span class="tag">dessert</span>
+                </div>
+                <h2>${recipe.name}</h2>
+                <span class="rating" role="img" aria-label="Rating: 4 out of 5 stars">
+                    <span aria-hidden="true" class="icon-star">⭐</span>
+                    <span aria-hidden="true" class="icon-star">⭐</span>
+                    <span aria-hidden="true" class="icon-star">⭐</span>
+                    <span aria-hidden="true" class="icon-star">⭐</span>
+                    <span aria-hidden="true" class="icon-star-empty">☆</span>
+                </span>
+            </section>
+        </article>`;
+}
+
+function ratingTemplate(rating) {
+	// begin building an html string using the ratings HTML written earlier as a model.
+	let html = `<span
+	class="rating"
+	role="img"
+	aria-label="Rating: ${rating} out of 5 stars"
+    >`
+// our ratings are always out of 5, so create a for loop from 1 to 5
+    for(i = 0; i < 5; i++) {
+		// check to see if the current index of the loop is less than our rating
+        if (i < rating) {
+           // if so then output a filled star 
+            html += `⭐`;
+        } else {
+            // else output an empty star
+            html += `☆`;
+        }
+    }
+		
+	// after the loop, add the closing tag to our string
+	html += `</span>`
+	// return the html string
+	return html
+}
+
+const recipe = getRandomListEntry(recipes);
+console.log(recipeTemplate(recipe));
